83e645696b25d0156da211a3e33f68a3bc33b0da
RR <newmsiartem@gmail.com>
2024-01-28 23:43:41 +0400

    add detectedLang

--- main.go
+++ main.go
@@ -1,87 +1,87 @@
-package main
+	package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"projects/BIb_bot/translate"
-	"strings"
+	import (
+		"fmt"
+		"log"
+		"os"
+		"projects/BIb_bot/translate"
+		"strings"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
-)
+		tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+		"github.com/joho/godotenv"
+	)
 
-var (
+	var (
 
-	msgText string
-)
-func main(){
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("[ERR] Error loading .env file")
-	}
-
-	token := os.Getenv("TOKEN_BOT")
-	
-
-	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		log.Fatal("[ERR] The bot has not been created")
-	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
-	
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		if update.Message.IsCommand(){
-		switch update.Message.Command(){
-		case "list":
-			msgText = "Здесь весь лист"
-		case "help":
-			msgText = "Тут помощь"	
-		case "tr":
-			text := strings.TrimSpace(update.Message.CommandArguments())
-				if text == "" {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Пожалуйста, введите текст для перевода после команды /tr.")
-					bot.Send(msg)
-					continue
-				}
-
-				log.Printf("[DEBUG] Переводим текст: %s", text)
-
-				translatedText, err := translate.TranslateText(text, "en", "ru")
-				if err != nil {
-					log.Printf("Error translating text: %v", err)
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Произошла ошибка при переводе текста.")
-					bot.Send(msg)
-				} else {
-					log.Printf("[DEBUG] Получен переведенный текст: %s", translatedText)
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Переведенный текст:\n%s", translatedText))
-					bot.Send(msg)
-				}
-		default :
-			msgText = "Nothing..."
-		}
-	}
-		userID := update.Message.Chat.ID
-		msg := tgbotapi.NewMessage(userID, msgText)
-
-		_, err := bot.Send(msg)
+		msgText string
+	)
+	func main(){
+		err := godotenv.Load()
 		if err != nil {
-			log.Printf("Error sending message: %v", err)
+			log.Fatal("[ERR] Error loading .env file")
 		}
+
+		token := os.Getenv("CHECK_BOT")
+		
+
+		bot, err := tgbotapi.NewBotAPI(token)
+		if err != nil {
+			log.Fatal("[ERR] The bot has not been created")
+		}
+
+		bot.Debug = true
+
+		log.Printf("Authorized on account %s", bot.Self.UserName)
+
+		
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+		updates := bot.GetUpdatesChan(u)
+		
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
+
+			if update.Message.IsCommand(){
+			switch update.Message.Command(){
+			case "list":
+				msgText = "Здесь весь лист"
+			case "help":
+				msgText = "Тут помощь"	
+			case "tr":
+				text := strings.TrimSpace(update.Message.CommandArguments())
+					if text == "" {
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Пожалуйста, введите текст для перевода после команды /tr.")
+						bot.Send(msg)
+						continue
+					}
+
+					log.Printf("[DEBUG] Переводим текст: %s", text)
+
+					translatedText, err := translate.TranslateText(text, "en", "ru")
+					if err != nil {
+						log.Printf("Error translating text: %v", err)
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Произошла ошибка при переводе текста.")
+						bot.Send(msg)
+					} else {
+						log.Printf("[DEBUG] Получен переведенный текст: %s", translatedText)
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Переведенный текст:\n%s", translatedText))
+						bot.Send(msg)
+					}
+			default :
+				msgText = "Nothing..."
+			}
+		}
+			userID := update.Message.Chat.ID
+			msg := tgbotapi.NewMessage(userID, msgText)
+
+			_, err := bot.Send(msg)
+			if err != nil {
+				log.Printf("Error sending message: %v", err)
+			}
+		}
+		
 	}
-	
-}
 
 
--- translate/func_translate.go
+++ translate/func_translate.go
@@ -10,7 +10,6 @@ import (
 	translate "cloud.google.com/go/translate/apiv3"
 	"cloud.google.com/go/translate/apiv3/translatepb"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
 )
 
 func init() {
@@ -20,69 +19,64 @@ func init() {
 	}
 }
 
+func detectLanguage(w io.Writer, text string, req *translatepb.DetectLanguageRequest) (string, error) {
+	projectID := os.Getenv("ID_PROJECT")
+	text = "Hello, world!"
 
-func detectLanguage(w io.Writer,  text string, req *translatepb.DetectLanguageRequest) (string,error) {
-	 projectID := os.Getenv("ID_PROJECT")
-	 text = "Hello, world!"
+	ctx := context.Background()
 
-	 ctx := context.Background()
-	 client, err := translate.NewTranslationClient(ctx)
-	 if err != nil {
-		 return "", fmt.Errorf("NewTranslationClient: %w", err)
-	 }
-
+	client, err := translate.NewTranslationClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("NewTranslationClient: %w", err)
+	}
 	defer client.Close()
 
 	req = &translatepb.DetectLanguageRequest{
-			Parent:   fmt.Sprintf("projects/%s/locations/global", projectID),
-			MimeType: "text/plain", // Mime types: "text/plain", "text/html"
-			Source: &translatepb.DetectLanguageRequest_Content{
-					Content: text,
-			},
+		Parent:   fmt.Sprintf("projects/%s/locations/global", projectID),
+		MimeType: "text/plain",
+		Source: &translatepb.DetectLanguageRequest_Content{
+			Content: text,
+		},
 	}
 
-	
-	  resp, err := client.DetectLanguage(ctx, req)
-    if err != nil {
-        return "", fmt.Errorf("DetectLanguage: %w", err)
-    }
+	resp, err := client.DetectLanguage(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("DetectLanguage: %w", err)
+	}
 
 	// Display list of detected languages sorted by detection confidence.
 	// The most probable language is first.
-	for _, language := range resp.GetLanguages() {
-			// The language detected.
-			fmt.Fprintf(w, "Language code: %v\n", language.GetLanguageCode())
-			// Confidence of detection result for this language.
-			fmt.Fprintf(w, "Confidence: %v\n", language.GetConfidence())
+	for _, language := range resp.Languages {
+		// The language detected.
+		fmt.Fprintf(w, "Language code: %v\n", language.GetLanguageCode())
+		// Confidence of detection result for this language.
+		fmt.Fprintf(w, "Confidence: %v\n", language.GetConfidence())
 	}
 
-	return "", fmt.Errorf("No languages detected")
-
+	return resp.Languages[0].GetLanguageCode(), nil
 }
-
-
-
 
 func TranslateText(text, sourceLang, targetLang string) (string, error) {
 	ctx := context.Background()
 
-	sourceLang, err := detectLanguage(nil,text, &translatepb.DetectLanguageRequest{})
+	// Set GOOGLE_APPLICATION_CREDENTIALS
+	err := os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\RR\\go\\src\\projects\\BIb_bot\\translation-api-412323-7e71139aef69.json")
 	if err != nil {
-		return "", fmt.Errorf("Error detecting language: %w", err)
+		log.Fatal("[ERR] Unable to set GOOGLE_APPLICATION_CREDENTIALS:", err)
 	}
 
-	translateAPIKey := os.Getenv("TOKEN_TRANSLATE")
-	
-
-
-	client, err := translate.NewTranslationClient(ctx, option.WithAPIKey(translateAPIKey))
+	client, err := translate.NewTranslationClient(ctx)
 	if err != nil {
 		log.Fatal("[ERR] Unable to create translation client:", err)
 	}
 	defer client.Close()
 
 	// Вызов Google Translate API
-	translation, err := client.TranslateText(ctx, &translatepb.TranslateTextRequest{})
+	translation, err := client.TranslateText(ctx, &translatepb.TranslateTextRequest{
+		Contents:       []string{text},
+		TargetLanguageCode: targetLang,
+		SourceLanguageCode: sourceLang,
+	})
 	if err != nil {
 		log.Fatal("[ERR] Unable to translate text:", err)
 		return "", err
@@ -90,6 +84,6 @@ func TranslateText(text, sourceLang, targetLang string) (string, error) {
 
 	fmt.Printf("[DEBUG] Google Translate API Response: %+v\n", translation)
 
-	return translation.Translations[0].String(), nil
+	return translation.Translations[0].GetTranslatedText(), nil
 }
-
+	
